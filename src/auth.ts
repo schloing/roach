420a1d60ab16ca60f5878a5fc8406adc446d51bb
schloing <user.datagram.protocol@proton.me>
2025-10-02 23:16:07 +1000

    fix uncommitted authjs setup

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,28 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
+import { SvelteKitAuth } from "@auth/sveltekit";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { providers } from "$/lib/auth_providers";
+import { db } from "$/lib/server/db";
+import { users, accounts, sessions } from "$/lib/db/schema";
+import { eq } from "drizzle-orm";
 
 export const { handle } = SvelteKitAuth({
-    providers: [],
+    providers: providers.map((obj) => obj.handler),
+    adapter: DrizzleAdapter(db, {
+        usersTable: users,
+        accountsTable: accounts,
+        sessionsTable: sessions,
+    }),
+    pages: {
+        error: "/error",
+    },
+    session: {
+        strategy: "database"
+    },
+    callbacks: {
+        async session({ session, user }) {
+            const [dbUser] = await db.select().from(users).where(eq(users.id, user.id)).limit(1);
+            session.user = dbUser;
+            return session;
+        },
+    },
 });
